5a5c874f0ec10d2b48da86ad1066e674ab85e769
Jakub Marciniszyn <lobin21@gmail.com>
2023-07-06 18:49:13 +0100

    Adds ldJson

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,54 @@ import '@app/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'HealthAndBeautyBusiness',
+  name: 'Gosia Massage',
+  image: 'https://gosiamassage.com/images/logo.svg',
+  '@id': 'https://gosiamassage.com/',
+  url: 'https://gosiamassage.com/',
+  telephone: '+447940158930',
+  priceRange: '££',
+  currenciesAccepted: 'GBP',
+  paymentAccepted: 'Cash, Credit Card, Debit Card',
+  address: {
+    '@type': 'PostalAddress',
+    streetAddress: '',
+    addressLocality: 'Wigan',
+    postalCode: '',
+    addressCountry: 'GB',
+  },
+  geo: {
+    '@type': 'GeoCoordinates',
+    latitude: 53.545067,
+    longitude: -2.632507,
+  },
+  areaServed: {
+    '@type': 'GeoCircle',
+    geoMidpoint: {
+      '@type': 'GeoCoordinates',
+      latitude: 53.545067,
+      longitude: -2.632507,
+    },
+    geoRadius: 32186,
+  },
+  openingHoursSpecification: [
+    {
+      '@type': 'OpeningHoursSpecification',
+      dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      opens: '17:00',
+      closes: '20:00',
+    },
+    {
+      '@type': 'OpeningHoursSpecification',
+      dayOfWeek: ['Saturday', 'Sunday'],
+      opens: '08:00',
+      closes: '20:00',
+    },
+  ],
+};
+
 export const metadata: Metadata = {
   title: {
     template: '%s | Gosia Massage',
@@ -26,6 +74,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Header />
         <main>{children}</main>
         <Footer />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
       </body>
     </html>
   );
