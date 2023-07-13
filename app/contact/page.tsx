72e43da2ebac11800771ff5ffca3661b15c99d1d
Jakub Marciniszyn <lobin21@gmail.com>
2023-07-13 13:43:41 +0100

    Adds service hours and list of towns

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -19,6 +19,21 @@ export default function ContactMe() {
         I, Gosia, your dedicated mobile massage therapist, am here to answer
         your questions and arrange your session.
       </p>
+
+      <h2>Service Hours</h2>
+      <p>
+        I&apos;m available to provide my services during the following hours:
+      </p>
+      <ul>
+        <li>Monday: 5pm to 8pm</li>
+        <li>Tuesday: 5pm to 8pm</li>
+        <li>Wednesday: 5pm to 8pm</li>
+        <li>Thursday: 5pm to 8pm</li>
+        <li>Friday: 5pm to 8pm</li>
+        <li>Saturday: 8am to 8pm</li>
+        <li>Sunday: 8am to 8pm</li>
+      </ul>
+
       <h2>How to Contact Me</h2>
       <p>
         SMS:{' '}
@@ -33,17 +48,52 @@ export default function ContactMe() {
         <br />
         WhatsApp: <a href={`https://wa.me/44${phone}`}>{`+44 ${phone}`}</a>
       </p>
+
       <h2>Areas I Serve</h2>
       <p>
-        I offer services in Wigan and surrounding areas within a 20-mile radius,
-        encompassing significant locales like Bolton, Leigh, and St Helens.
+        I offer services within a 20-mile radius of Wigan, including but not
+        limited to the following locations:
       </p>
+      <ul>
+        <li>Wigan</li>
+        <li>Leigh</li>
+        <li>Tyldesley</li>
+        <li>Skelmersdale</li>
+        <li>Saint Helens</li>
+        <li>Bolton</li>
+        <li>Great Sankey</li>
+        <li>Warrington</li>
+        <li>Prescot</li>
+        <li>Kirkby</li>
+        <li>Radcliffe</li>
+        <li>Eccles</li>
+        <li>Urmston</li>
+        <li>Bury</li>
+        <li>Widnes</li>
+        <li>Stretford</li>
+        <li>Blackburn</li>
+        <li>Runcorn</li>
+        <li>Salford</li>
+        <li>Preston</li>
+        <li>Altrincham</li>
+        <li>Sale</li>
+        <li>Manchester</li>
+        <li>Crosby</li>
+        <li>Southport</li>
+        <li>Middleton</li>
+        <li>Liverpool</li>
+        <li>Birkenhead</li>
+        <li>Wallasey</li>
+        <li>Northwich</li>
+      </ul>
+
       <h2>Scheduling an Appointment</h2>
       <p>
         To discuss your specific needs and set up a massage session, please
         reach out via SMS or WhatsApp. I&apos;m available for home visits
         throughout the week.
       </p>
+
       <h2>Questions and Feedback</h2>
       <p>
         I value your feedback and am available to clarify any queries you might
