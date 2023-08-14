a2dd2c79d72540cf4ad3fabcdc37696825184b62
Jakub Marciniszyn <71651055+lbn21@users.noreply.github.com>
2023-08-14 09:07:04 +0100

    Adds new review

--- app/data/Reviews.ts
+++ app/data/Reviews.ts
@@ -12,6 +12,16 @@ export type Review = {
 
 export const REVIEWS: Review[] = [
   {
+    id: 3,
+    name: 'John',
+    content:
+      'I highly recommend gosia massage, i am a sufferer of sciatica in my back and both legs, I also have an old shoulder injury that effects not only my neck but my arms aswell. Gosia helps me with my day to day working life with the massage that\'s tailored for me without her caring and willingness to listen to my needs I wouldn\'t be able to work my job which I love. For a truly professional massage therapist gosia is by far the best massage I\'ve ever received.',
+    rating: 5,
+    date: new Date(1690708758000),
+    avatar: generateColorsAndInitials('John'),
+    link: 'https://g.co/kgs/xArbBr',
+  },
+  {
     id: 2,
     name: 'John Harvey',
     content:
